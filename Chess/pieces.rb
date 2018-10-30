6de74bfa6c23ec314ab7b27925d1a272a5dde80e
sameehkhan <sameehkhan1@gmail.com>
2018-10-30 14:02:46 -0700

    Add chess symbols for all piece classes

--- Chess/pieces.rb
+++ Chess/pieces.rb
@@ -28,9 +28,32 @@ class Piece
   end
 
   private
-    
+
   def move_into_check?(end_pos)
 
   end
+
+end
+
+
+class Pawn < Piece
+end
+
+class Rook < Piece
+end
+
+class Bishop < Piece
+end
+
+class Queen < Piece
+end
+
+class Knight < Piece
+end
+
+class King < Piece
+end
+
+class NullPiece < Piece 
 
 end
