5012e7b06ee75dbd6340dc5187c924e0a71239d0
sameehkhan <sameehkhan1@gmail.com>
2018-10-30 14:15:11 -0700

    Add Slideable and Stepable module skeletons

--- Chess/pieces.rb
+++ Chess/pieces.rb
@@ -1,4 +1,5 @@
 class Piece
+attr_reader :color, :pos
 
   def initialize(color, board, pos)
     @color = color
@@ -6,6 +7,7 @@ class Piece
     @pos = []
 
   end
+
 
   def to_s
 
@@ -37,23 +39,118 @@ end
 
 
 class Pawn < Piece
+  def symbol
+    if self.color == :black
+      " ♟ "
+    else
+      ' ♙ '
+    end
+  end
+
+  def move_dirs
+
+  end
+
+  private
+
+  def at_start_row?
+  end
+
+  def forward_dir
+  end
+
+  def forward_steps
+  end
+
+  def side_attacks
+  end
+
 end
 
 class Rook < Piece
+  def symbol
+    if self.color == :black
+      " ♜ "
+    else
+      ' ♖ '
+  end
 end
 
 class Bishop < Piece
+  def symbol
+    if self.color == :black
+      " ♝ "
+    else
+      ' ♗ '
+    end
+  end
 end
 
 class Queen < Piece
+  def symbol
+    if self.color == :black
+      " ♛ "
+    else
+      " ♕ "
+    end
+  end
 end
 
 class Knight < Piece
+  def symbol
+    if self.color == :black
+      " ♞ "
+    else
+      " ♘ "
+   end
+  end
 end
 
 class King < Piece
+  def symbol
+    if self.color == :black
+      " ♚ "
+    else
+      ' ♔ '
+    end
+  end
 end
 
-class NullPiece < Piece 
+class NullPiece < Piece
+  def symbol
+    " 🀫 "
+  end
+end
+
+module SlidingPiece
+
+  def moves
+  end
+
+  def horizontal_dirs
+  end
+
+  def diagonal_dirs
+
+  end
+
+  private
+  def move_dirs
+  end
+
+  def grow_unblocked_moves_in_dir(dx,dy)
+  end
+
+end
+
+module SteppingPiece
+
+  def moves
+  end
+
+  private
+  def move_diffs
+
+  end
 
 end
